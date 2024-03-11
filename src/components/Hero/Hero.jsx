e41530baf4c187956994fd5bf62860a5316e3d2e
MuhammadImaz <imaz.anwar01@gmail.com>
2024-03-12 00:50:11 +0500

    Update-2 portfolio

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ export const Hero = () => {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <h1 className={styles.title}>Hi, My name is </h1>
+        <h1 className={styles.titles}>Hi, My name is </h1>
         <h2 className={styles.title}> Muhmmad Imaz</h2>
         <p className={styles.description}>
           I am an enthusiast web developer with expertise in React and
